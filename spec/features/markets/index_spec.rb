a3d4d1b9e3b3d0d6ccee207a23cdbbd93a226165
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-05-23 10:46:39 -0400

    Refactor us1 iteration for first 3 only to speed up test

--- spec/features/markets/index_spec.rb
+++ spec/features/markets/index_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
-
 RSpec.describe "Market index page", type: :feature do
   it "Shows all markets with name, city, and state" do
     VCR.use_cassette("market_index_data", serialize_with: :json) do |cassette|
@@ -18,16 +17,16 @@ RSpec.describe "Market index page", type: :feature do
       within '.markets' do
         expect(page).to have_selector('li.market')
 
-        markets.each do |market_data|
+        markets.first(3).each do |market_data|
           market_attributes = market_data[:attributes]
           
-          name = (market_attributes[:name]).gsub(/\s+/, ' ').strip
+          name = market_attributes[:name].gsub(/\s+/, ' ').strip
           city = market_attributes[:city].to_s.gsub(/\s+/, ' ').strip
           state = market_attributes[:state].gsub(/\s+/, ' ').strip
           
-          expect(page).to have_link(name) unless name.empty?
-          expect(page).to have_content(city) unless name.empty?
-          expect(page).to have_content(state) unless name.empty?
+          expect(page).to have_link(name)
+          expect(page).to have_content(city)
+          expect(page).to have_content(state)
         end
       end
     end 
