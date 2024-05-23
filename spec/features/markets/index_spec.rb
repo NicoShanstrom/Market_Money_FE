73778d517af2e559cccce12b843be9333d3fa3a8
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-05-23 00:31:29 -0400

    Different way to parse the data and look into it using a cassette

--- spec/features/markets/index_spec.rb
+++ spec/features/markets/index_spec.rb
@@ -1,33 +1,35 @@
 require 'rails_helper'
-require 'yaml'
 
-RSpec.describe "Market index page", type: :feature, vcr: true do
-  before(:each) do
-    VCR.use_cassette("market_index_data") do
-      @markets_data = MarketMoneyApiFacade.get_markets["data"]
-    end
-  end
 
+RSpec.describe "Market index page", type: :feature do
   it "Shows all markets with name, city, and state" do
-    visit markets_path
-    
-    expect(page.status_code).to eq(200)
-  
-    within '.markets' do
-      expect(page).to have_selector('li.market')
+    VCR.use_cassette("market_index_data", serialize_with: :json) do |cassette|
+      visit markets_path
 
-      @markets_data.each do |market_data|
-        market_attributes = market_data["attributes"]
-        
-        name = (market_attributes["name"]).gsub(/\s+/, ' ').strip
-        city = (market_attributes["city"]).to_s.gsub(/\s+/, ' ').strip
-        state = (market_attributes["state"]).gsub(/\s+/, ' ').strip
-        
-        expect(page).to have_link(name)
-        expect(page).to have_content(city)
-        expect(page).to have_content(state)
+      expect(page.status_code).to eq(200)
+
+      body = JSON.parse(
+        cassette.serializable_hash.dig("http_interactions", 0, "response", "body", "string"),
+        symbolize_names: true
+      )
+
+      markets = body[:data]
+    
+      within '.markets' do
+        expect(page).to have_selector('li.market')
+
+        markets.each do |market_data|
+          market_attributes = market_data[:attributes]
+          
+          name = (market_attributes[:name]).gsub(/\s+/, ' ').strip
+          city = market_attributes[:city].to_s.gsub(/\s+/, ' ').strip
+          state = market_attributes[:state].gsub(/\s+/, ' ').strip
+          
+          expect(page).to have_link(name) unless name.empty?
+          expect(page).to have_content(city) unless name.empty?
+          expect(page).to have_content(state) unless name.empty?
+        end
       end
-    end
-    # end
+    end 
   end
 end
