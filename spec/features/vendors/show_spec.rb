b606b71f6f832755b39e5b7ef2e97e23d6dd4f80
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-05-23 20:00:50 -0400

    More whitespace method additions

--- spec/features/vendors/show_spec.rb
+++ spec/features/vendors/show_spec.rb
@@ -6,11 +6,12 @@ RSpec.describe "Market show page", type: :feature do
     visit vendor_path(vendor_id)
 
     vendor_attributes = vendor[:attributes]
-    name = vendor_attributes[:name].strip_whitespace
-    contact_name = vendor_attributes[:contact_name].strip_whitespace
-    contact_phone = vendor_attributes[:contact_phone].strip_whitespace
-    credit_accepted = vendor_attributes[:credit_accepted].strip_whitespace
-    description = vendor_attributes[:description].strip_whitespace
+    name = strip_whitespace(vendor_attributes[:name])
+    contact_name = strip_whitespace(vendor_attributes[:contact_name])
+    contact_phone = strip_whitespace(vendor_attributes[:contact_phone])
+    credit_accepted = strip_whitespace(vendor_attributes[:credit_accepted])
+    description = strip_whitespace(vendor_attributes[:description])
+    
     expect(page).to have_content("vendor.name")
     expect(page).to have_content("Contact Info:")
     expect(page).to have_content("Name: vendor.contact_name")
